4e481ecbc5d5e846865134d5d0c8c09ca18ea02a
AbdessamadAe <ab.aitelmouden@gmail.com>
2023-09-30 11:17:23 +0200

    first

--- js/read-more.js
+++ js/read-more.js
@@ -1,19 +1,68 @@
-  $(document).ready(function() {
-    $("#read-more-btn").click(function() {
-      var elem = $("#read-more-btn").text();
-      if (elem == "Read More") {
-        //Stuff to do when btn is in the read more state
-        $("#read-more-btn").text("Read Less");
-        $("#more").slideDown();
-      } else {
-        //Stuff to do when btn is in the read less state
-        $("#read-more-btn").text("Read More");
-        $("#more").slideUp();
-      }
-    });
+$(document).ready(function() {
+  $("#read-more-btn").click(function() {
+    var elem = $("#read-more-btn").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#read-more-btn").text("Read Less");
+      $("#more").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#read-more-btn").text("Read More");
+      $("#more").slideUp();
+    }
   });
+});
 
-  function toggleList(element) {
-    const subList = element.nextElementSibling;
-    subList.style.display = subList.style.display === 'none' ? 'block' : 'none';
-  }  
+
+//header in read more section
+$(document).ready(function() {
+  $("#list-header2").click(function() {
+    const subList = $(this).next();
+    const buttonText = $(this).text();
+    
+    if (buttonText.includes("Read More")) {
+      $(this).text(buttonText.replace("Read More", "Read Less"));
+      subList.show();
+    } else {
+      $(this).text(buttonText.replace("Read Less", "Read More"));
+      subList.hide();
+    }
+  });
+});
+
+//header in Call For Papers page
+$(document).ready(function() {
+  const subList = $("#list-strands").next();
+  subList.hide();
+
+  $("#list-strands").click(function() {
+    const buttonText = $(this).text();
+    
+    if (buttonText.includes("Read More")) {
+      subList.show();
+      $(this).text(buttonText.replace("Read More", "Read Less"));
+    } else {
+      $(this).text(buttonText.replace("Read Less", "Read More"));
+      subList.hide();
+    }
+  });
+});
+
+//For arabic list
+$(document).ready(function() {
+  const subList = $("#list-header-ar").next();
+  subList.hide();
+
+  $("#list-header-ar").click(function() {
+    const buttonText = $(this).text();
+    
+    if (buttonText.includes("إقرأ المزيد")) {
+      subList.show();
+      $(this).text(buttonText.replace("إقرأ المزيد", "إقرأ أقل"));
+    } else {
+      $(this).text(buttonText.replace("إقرأ أقل", "إقرأ المزيد"));
+      subList.hide();
+    }
+  });
+});
+
